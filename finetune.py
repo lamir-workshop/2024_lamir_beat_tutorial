0669305995427cb90e2b3072ab62696f887e2220
Giovana Morais <giovana.vmorais@gmail.com>
2024-12-16 23:37:47 -0300

    add num_files for loop

--- finetune.py
+++ finetune.py
@@ -31,45 +31,46 @@ if __name__ == "__main__":
     train_keys, test_keys = train_test_split(dataset_keys, test_size=0.2, random_state=42)
     train_keys, val_keys = train_test_split(train_keys, test_size=0.25, random_state=42)
 
-    # create dataloaders with different amount of files and evaluate them
-    # for num_files in range(1, PARAMS["MAX_NUM_FILES"]+1):
-    train_data = BeatData(dataset_tracks, train_keys, widen=True)
-    val_data = BeatData(dataset_tracks, val_keys, widen=True)
-    test_data = BeatData(dataset_tracks, test_keys, widen=True)
+    for num_files in range(1, PARAMS["MAX_NUM_FILES"]+1):
+        # create dataloaders with different amount of files and evaluate them
+        print(f"TRAIN WITH {num_files} FILES")
+        train_data = BeatData(dataset_tracks, train_keys[:num_files], widen=True)
+        val_data = BeatData(dataset_tracks, val_keys[:num_files], widen=True)
+        test_data = BeatData(dataset_tracks, test_keys, widen=True)
 
-    train_dataloader = DataLoader(train_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
-    val_dataloader = DataLoader(val_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
-    test_dataloader = DataLoader(test_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
+        train_dataloader = DataLoader(train_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
+        val_dataloader = DataLoader(val_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
+        test_dataloader = DataLoader(test_data, batch_size=1, num_workers=PARAMS["NUM_WORKERS"])
 
-    # instantiate model
-    tcn = MultiTracker(
-        n_filters=PARAMS["N_FILTERS"],
-        n_dilations=PARAMS["N_DILATIONS"],
-        kernel_size=PARAMS["KERNEL_SIZE"],
-        dropout_rate=PARAMS["DROPOUT"]
-    )
+        # instantiate model
+        tcn = MultiTracker(
+            n_filters=PARAMS["N_FILTERS"],
+            n_dilations=PARAMS["N_DILATIONS"],
+            kernel_size=PARAMS["KERNEL_SIZE"],
+            dropout_rate=PARAMS["DROPOUT"]
+        )
 
-    # load model checkpoint
-    model = PLTCN.load_from_checkpoint(
-        f"trained_models/{PARAMS['CHECKPOINT_FILE']}.ckpt",
-        model=tcn,
-        params=PARAMS
-    )
+        # load model checkpoint
+        model = PLTCN.load_from_checkpoint(
+            f"trained_models/{PARAMS['CHECKPOINT_FILE']}.ckpt",
+            model=tcn,
+            params=PARAMS
+        )
 
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    run = wandb.init(
-        project="LAMIR_beat_tutorial",
-        name=f"TCN_brid_finetuning_{timestamp}",
-        config=PARAMS
-    )
-    logger = WandbLogger()
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        run = wandb.init(
+            project="LAMIR_beat_tutorial",
+            name=f"TCN_brid_finetuning_{num_files}_{timestamp}",
+            config=PARAMS
+        )
+        logger = WandbLogger()
 
-    trainer = L.Trainer(
-        logger=logger,
-        max_epochs=PARAMS["N_EPOCHS"]
-    )
+        trainer = L.Trainer(
+            logger=logger,
+            max_epochs=PARAMS["N_EPOCHS"]
+        )
 
-    trainer.fit(model, train_dataloader, val_dataloader)
-    trainer.test(model, test_dataloader, verbose=True)
+        trainer.fit(model, train_dataloader, val_dataloader)
+        trainer.test(model, test_dataloader, verbose=True)
 
-    run.finish()
+        run.finish()
